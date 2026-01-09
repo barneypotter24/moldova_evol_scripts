a0714a50392b0eb3f2f11545d24e408128df9b26
Barney Potter <barney.potter.24@gmail.com>
2026-01-09 00:38:41 -0500

    update snakemake submission script

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -3,11 +3,12 @@
 #SBATCH --job-name=snakemake-main-job
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=10:00:00
+#SBATCH --mem=2G
 #SBATCH --output=slurm_logs/%x-%j.log
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=Array_TBAssemble
-#SBATCH --array=0-5:1
+#SBATCH --job-name=Array_TBAssemble_uncombined
+#SBATCH --array=112-227:1
 
 echo "Slurm job ID SLURM_ARRAY_JOB_ID is ${SLURM_ARRAY_JOB_ID}"
 echo "Slurm job array index SLURM_ARRAY_TASK_ID is ${SLURM_ARRAY_TASK_ID}"
@@ -19,7 +20,7 @@ conda activate
 echo "activating conda environment"
 conda activate tb-assemble
 
-CFG_FILE=Batch3/config/2_combined/config${SLURM_ARRAY_TASK_ID}.yaml
+CFG_FILE=config/config${SLURM_ARRAY_TASK_ID}.yaml
 echo "Configuration file CFG_FILE is ${CFG_FILE}"
 
 mkdir -p slurm_logs
@@ -28,5 +29,5 @@ export SBATCH_DEFAULTS=" --output=slurm_logs/%x-%j.log"
 # tb-profiler update_tbdb --match_ref H37Rv.fasta ## run this if tb-profiler is failing to match locations
 
 date
-time snakemake -s Snakefile_combined --cores all --use-conda -j 1 -w 30 --rerun-incomplete --rerun-triggers mtime --configfile ${CFG_FILE} --nolock
+time snakemake -s Snakefile_align --cores all --use-conda -j 1 -w 30 --rerun-incomplete --rerun-triggers mtime --configfile ${CFG_FILE} --nolock
 date
